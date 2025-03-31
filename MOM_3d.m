d727e7db4e1f6db47ae64987ebff8b4633312fb9
Jack4891 <95482991+Jack4891@users.noreply.github.com>
2025-03-31 16:52:34 -0400

    finished?

--- MOM_3d.m
+++ MOM_3d.m
@@ -10,14 +10,14 @@ mu0 = 4*pi*1e-7;
 e0 = 8.854e-12;
 
 k0 = w0 * sqrt(mu0*e0);
-% L = 20 * Lambda;
-% W = 10 * Lambda;
-L = 2;
-W = 3;
+L = 3 * Lambda;
+W = 3 * Lambda;
+% L = 2;
+% W = 3;
 
 
 %% generate the mesh
-n = 3; % number of elements along the x axis
+n = 15; % number of elements along the x axis
 x = linspace(-L/2,L/2,n);
 y = linspace(-W/2,W/2,n);
 
@@ -146,11 +146,16 @@ for m = 1:size(Edges,2)
             repmat(QRuleS(:,2),[1,3]).'.*repmat(V2nm,[1,numQRuleS])+...
             repmat(QRuleS(:,3),[1,3]).'.*repmat(V3nm,[1,numQRuleS]);
         RHOn_Minus = rqm - repmat(freeVertexNeg(:,n),[1,numQRuleS]); 
+        
+        RHO_Saved_Minus(:,n) = RHOn_Minus;
     
         rqp = repmat(QRuleS(:,1),[1,3]).'.*repmat(V1np,[1,numQRuleS])+...
             repmat(QRuleS(:,2),[1,3]).'.*repmat(V2np,[1,numQRuleS])+...
             repmat(QRuleS(:,3),[1,3]).'.*repmat(V3np,[1,numQRuleS]);
         RHOn_Plus = repmat(freeVertexPos(:,n),[1,numQRuleS]) - rqp; 
+
+        RHO_Saved_Plus(:,n) = RHOn_Plus;
+
 
         %% Tm- and Tn-
         centroid_Tmm = 1/3 * sum(p(:,Triangles(1:3,negTri(m))),2);
@@ -271,13 +276,64 @@ for m = 1:size(Edges,2)
 
         %% ZMN
         Zmn(m,n) = TmmTnm + TmmTnp + TmpTnp + TmpTnm;
+    end     
 
+
+    %% generation of Vm
+    IncTheta=45*pi/180;
+    IncPhi=45*pi/180;
+    IncK=[sin(IncTheta)*cos(IncPhi) sin(IncTheta)*sin(IncPhi) ... 
+    cos(IncTheta)]; 
+    IncPol=[0;1;0];
+    E0i=1; 
+    Einc=@(x,y,z)E0i.*IncPol.*exp(-1i.*dot(k0.*IncK,[x,y,z]));
+
+
+    V(m,1)=sum((edgeLength(m)./2).*QRuleO(:,4).'.*dot(RHOm_Minus,Einc(rpm(1,:),rpm(2,:),rpm(3,:))))+...
+        sum((edgeLength(m)./2).*QRuleO(:,4).'.*dot(RHOm_Plus,Einc(rpp(1,:),rpp(2,:),rpp(3,:))));
+end
+
+%% currents
+I = inv(Zmn) * V
+
+%% plot surface current densities
+Index=find(Triangles(4,:)<=1);
+numTriangles=length(Index);
+
+for k = 1:numTriangles
+    J = [0 0 0];
+    for m = 1:edgesTotal
+        IE = I(m) * edgeLength(m);
+        if(posTri(m) == k)
+            J = J + IE * RHO_Saved_Plus(:,m) / (2*Area(posTri(m)));
+        end
+        if(negTri(m) == k)
+            J = J + IE * RHO_Saved_Minus(:,m) / (2*Area(posTri(m)));
+        end
+        Jx = J(1);
+        Jy = J(2);
+        Jz = J(3);
+        normalCurrent(k) = abs(norm(J));
 
 
     end
-        
 end
 
+Jmax=max(normalCurrent);
+MaxCurrent=strcat(num2str(Jmax),'[A/m]')
+CurrentNorm1=normalCurrent./max(normalCurrent);
+for m=1:numTriangles
+    N=Triangles(1:3,m);
+    Xp(1:3,m)=[p(1,N)]';
+    Yp(1:3,m)=[p(2,N)]';
+    Zp(1:3,m)=[p(3,N)]';
+end
+Cam=repmat(CurrentNorm1,3,1);
+Cph=repmat(Jy./max(normalCurrent),3,1);
 
+h = fill3(Xp,Yp,Zp,Cam);
+colormap jet;
+axis('equal')
 
-
+xlim([-L L]) 
+ylim([-W W])
